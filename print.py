dce6d126ca7c94450de5236d8e92c1c239305841
Luke Clark <lukeclarksfo@gmail.com>
2024-07-09 12:37:43 +0200

    Update

---
+++ print.py
@@ -0,0 +1,15 @@
+# pip install numpy pandas scikit-learn tensorflow keras torch maplotlib
+import numpy as np
+import pandas as pandas
+import sklearn as sklearn
+import tensorflow as tensorflow
+import torch as torch
+import matplotlib as matplotlib
+
+array = [1,2,3]
+
+np_array = np.array(array)
+
+print(type(np_array))
+
+print("The program works.")
