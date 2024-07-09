06032cb0e14956374d2a5eb24804229e80d1f56d
Luke Clark <lukeclarksfo@gmail.com>
2024-07-09 13:11:13 +0200

    Update sklearn

--- print.py
+++ print.py
@@ -1,10 +1,10 @@
 # pip install numpy pandas scikit-learn tensorflow keras torch maplotlib
 import numpy as np
-import pandas as pandas
+#import pandas as pandas
 import sklearn as sklearn
-import tensorflow as tensorflow
-import torch as torch
-import matplotlib as matplotlib
+#import tensorflow as tensorflow
+#import torch as torch
+#import matplotlib as matplotlib
 
 array = [1,2,3]
 
@@ -13,3 +13,14 @@ np_array = np.array(array)
 print(type(np_array))
 
 print("The program works.")
+
+from sklearn.datasets import load_iris
+iris_dataset = load_iris()
+
+print("Keys of iris dataset: \n{}".format(iris_dataset.keys()))
+
+print(iris_dataset['DESCR'][:193] + "\n...")
+
+print("Target names: {}".format(iris_dataset['target_names']))
+
+print("Feature names: \n{}".format(iris_dataset['feature_names']))
